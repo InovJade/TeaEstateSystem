db4cee66fed33cdb812744ff5538535ed7acb47d
Inovjade <amarajeewaesindu@gmail.com>
2026-07-05 16:36:06 +0530

    Digital Tea Estate Workforce Management System

--- TeaEstateSystem/WorkersForm.Designer.cs
+++ TeaEstateSystem/WorkersForm.Designer.cs
@@ -32,137 +32,149 @@
             this.txtNIC = new System.Windows.Forms.TextBox();
             this.txtPhone = new System.Windows.Forms.TextBox();
             this.txtAddress = new System.Windows.Forms.TextBox();
+            this.txtFullName1 = new System.Windows.Forms.Label();
+            this.txtNIC1 = new System.Windows.Forms.Label();
+            this.txtPhone1 = new System.Windows.Forms.Label();
+            this.txtAddress1 = new System.Windows.Forms.Label();
             this.dtpJoined = new System.Windows.Forms.DateTimePicker();
             this.btnSave = new System.Windows.Forms.Button();
             this.dgvWorkers = new System.Windows.Forms.DataGridView();
-            this.dj = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dgvWorkers)).BeginInit();
             this.SuspendLayout();
             // 
             // txtFullName
             // 
-            this.txtFullName.Location = new System.Drawing.Point(295, 78);
+            this.txtFullName.Location = new System.Drawing.Point(219, 32);
             this.txtFullName.Name = "txtFullName";
             this.txtFullName.Size = new System.Drawing.Size(100, 20);
             this.txtFullName.TabIndex = 0;
-            this.txtFullName.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // txtNIC
             // 
-            this.txtNIC.Location = new System.Drawing.Point(295, 146);
+            this.txtNIC.Location = new System.Drawing.Point(219, 58);
             this.txtNIC.Name = "txtNIC";
             this.txtNIC.Size = new System.Drawing.Size(100, 20);
             this.txtNIC.TabIndex = 1;
             // 
             // txtPhone
             // 
-            this.txtPhone.Location = new System.Drawing.Point(295, 215);
+            this.txtPhone.Location = new System.Drawing.Point(219, 84);
             this.txtPhone.Name = "txtPhone";
             this.txtPhone.Size = new System.Drawing.Size(100, 20);
             this.txtPhone.TabIndex = 2;
             // 
             // txtAddress
             // 
-            this.txtAddress.Location = new System.Drawing.Point(295, 273);
+            this.txtAddress.Location = new System.Drawing.Point(219, 110);
             this.txtAddress.Name = "txtAddress";
             this.txtAddress.Size = new System.Drawing.Size(100, 20);
             this.txtAddress.TabIndex = 3;
             // 
+            // txtFullName1
+            // 
+            this.txtFullName1.AutoSize = true;
+            this.txtFullName1.Location = new System.Drawing.Point(143, 39);
+            this.txtFullName1.Name = "txtFullName1";
+            this.txtFullName1.Size = new System.Drawing.Size(54, 13);
+            this.txtFullName1.TabIndex = 4;
+            this.txtFullName1.Text = "Full Name";
+            // 
+            // txtNIC1
+            // 
+            this.txtNIC1.AutoSize = true;
+            this.txtNIC1.Location = new System.Drawing.Point(143, 65);
+            this.txtNIC1.Name = "txtNIC1";
+            this.txtNIC1.Size = new System.Drawing.Size(25, 13);
+            this.txtNIC1.TabIndex = 5;
+            this.txtNIC1.Text = "NIC";
+            // 
+            // txtPhone1
+            // 
+            this.txtPhone1.AutoSize = true;
+            this.txtPhone1.Location = new System.Drawing.Point(143, 91);
+            this.txtPhone1.Name = "txtPhone1";
+            this.txtPhone1.Size = new System.Drawing.Size(38, 13);
+            this.txtPhone1.TabIndex = 6;
+            this.txtPhone1.Text = "Phone";
+            // 
+            // txtAddress1
+            // 
+            this.txtAddress1.AutoSize = true;
+            this.txtAddress1.Location = new System.Drawing.Point(143, 117);
+            this.txtAddress1.Name = "txtAddress1";
+            this.txtAddress1.Size = new System.Drawing.Size(45, 13);
+            this.txtAddress1.TabIndex = 7;
+            this.txtAddress1.Text = "Address";
+            // 
             // dtpJoined
             // 
-            this.dtpJoined.Location = new System.Drawing.Point(524, 94);
+            this.dtpJoined.Location = new System.Drawing.Point(466, 59);
             this.dtpJoined.Name = "dtpJoined";
             this.dtpJoined.Size = new System.Drawing.Size(200, 20);
-            this.dtpJoined.TabIndex = 4;
+            this.dtpJoined.TabIndex = 8;
             // 
             // btnSave
             // 
-            this.btnSave.Location = new System.Drawing.Point(306, 361);
+            this.btnSave.Location = new System.Drawing.Point(370, 409);
             this.btnSave.Name = "btnSave";
-            this.btnSave.Size = new System.Drawing.Size(75, 23);
-            this.btnSave.TabIndex = 5;
-            this.btnSave.Text = "Save";
+            this.btnSave.Size = new System.Drawing.Size(75, 29);
+            this.btnSave.TabIndex = 9;
+            this.btnSave.Text = "save";
             this.btnSave.UseVisualStyleBackColor = true;
-            this.btnSave.Click += new System.EventHandler(this.button1_Click);
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click_1);
             // 
             // dgvWorkers
             // 
             this.dgvWorkers.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvWorkers.Location = new System.Drawing.Point(506, 143);
+            this.dgvWorkers.Location = new System.Drawing.Point(146, 202);
             this.dgvWorkers.Name = "dgvWorkers";
-            this.dgvWorkers.Size = new System.Drawing.Size(240, 150);
-            this.dgvWorkers.TabIndex = 6;
+            this.dgvWorkers.Size = new System.Drawing.Size(529, 181);
+            this.dgvWorkers.TabIndex = 10;
+            this.dgvWorkers.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvWorkers_CellContentClick);
             // 
-            // dj
+            // btnUpdate
             // 
-            this.dj.AutoSize = true;
-            this.dj.Location = new System.Drawing.Point(573, 78);
-            this.dj.Name = "dj";
-            this.dj.Size = new System.Drawing.Size(92, 13);
-            this.dj.TabIndex = 7;
-            this.dj.Text = "Enter Date Joined";
+            this.btnUpdate.Location = new System.Drawing.Point(476, 110);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 23);
+            this.btnUpdate.TabIndex = 11;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
             // 
-            // label1
+            // btnDelete
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(218, 81);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(51, 13);
-            this.label1.TabIndex = 8;
-            this.label1.Text = "FullName";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(218, 149);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(41, 13);
-            this.label2.TabIndex = 9;
-            this.label2.Text = "NIC/ID";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(201, 218);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(78, 13);
-            this.label3.TabIndex = 10;
-            this.label3.Text = "Phone Number";
-            this.label3.Click += new System.EventHandler(this.label3_Click);
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(218, 276);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(39, 13);
-            this.label4.TabIndex = 11;
-            this.label4.Text = "Adress";
+            this.btnDelete.Location = new System.Drawing.Point(476, 139);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 12;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
             // 
             // WorkersForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.dj);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
             this.Controls.Add(this.dgvWorkers);
             this.Controls.Add(this.btnSave);
             this.Controls.Add(this.dtpJoined);
+            this.Controls.Add(this.txtAddress1);
+            this.Controls.Add(this.txtPhone1);
+            this.Controls.Add(this.txtNIC1);
+            this.Controls.Add(this.txtFullName1);
             this.Controls.Add(this.txtAddress);
             this.Controls.Add(this.txtPhone);
             this.Controls.Add(this.txtNIC);
             this.Controls.Add(this.txtFullName);
             this.Name = "WorkersForm";
             this.Text = "WorkersForm";
+            this.Load += new System.EventHandler(this.WorkersForm_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dgvWorkers)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -175,13 +187,14 @@
         private System.Windows.Forms.TextBox txtNIC;
         private System.Windows.Forms.TextBox txtPhone;
         private System.Windows.Forms.TextBox txtAddress;
+        private System.Windows.Forms.Label txtFullName1;
+        private System.Windows.Forms.Label txtNIC1;
+        private System.Windows.Forms.Label txtPhone1;
+        private System.Windows.Forms.Label txtAddress1;
         private System.Windows.Forms.DateTimePicker dtpJoined;
         private System.Windows.Forms.Button btnSave;
         private System.Windows.Forms.DataGridView dgvWorkers;
-        private System.Windows.Forms.Label dj;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
     }
 }
--- TeaEstateSystem/WorkersForm.cs
+++ TeaEstateSystem/WorkersForm.cs
@@ -13,34 +13,64 @@ namespace TeaEstateSystem
 {
     public partial class WorkersForm : Form
     {
+        DBConnection db = new DBConnection();
+
         public WorkersForm()
         {
             InitializeComponent();
         }
 
-        private void textBox1_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button1_Click(object sender, EventArgs e)
+        private void LoadWorkers()
         {
             try
             {
-                DBConnection db = new DBConnection();
-
                 using (SqlConnection conn = db.GetConnection())
                 {
                     conn.Open();
 
-                    string query =
-                        "INSERT INTO Workers " +
-                        "(FullName,NIC,Phone,Address,DateJoined) " +
-                        "VALUES " +
-                        "(@name,@nic,@phone,@address,@date)";
+                    SqlDataAdapter da =
+                        new SqlDataAdapter("SELECT * FROM Workers", conn);
 
-                    SqlCommand cmd =
-                        new SqlCommand(query, conn);
+                    DataTable dt = new DataTable();
+                    da.Fill(dt);
+
+                    dgvWorkers.DataSource = dt;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Load Error: " + ex.Message);
+            }
+        }
+
+
+        private void WorkersForm_Load(object sender, EventArgs e)
+        {
+            LoadWorkers();
+        }
+
+        private void ClearFields()
+        {
+            txtFullName.Clear();
+            txtNIC.Clear();
+            txtPhone.Clear();
+            txtAddress.Clear();
+        }
+
+        private void btnSave_Click_1(object sender, EventArgs e)
+        {
+            try
+            {
+                using (SqlConnection conn = db.GetConnection())
+                {
+                    conn.Open();
+
+                    string query = @"INSERT INTO Workers
+                                    (FullName, NIC, Phone, Address, DateJoined)
+                                    VALUES
+                                    (@name, @nic, @phone, @address, @date)";
+
+                    SqlCommand cmd = new SqlCommand(query, conn);
 
                     cmd.Parameters.AddWithValue("@name", txtFullName.Text);
                     cmd.Parameters.AddWithValue("@nic", txtNIC.Text);
@@ -51,6 +81,61 @@ namespace TeaEstateSystem
                     cmd.ExecuteNonQuery();
 
                     MessageBox.Show("Worker Saved");
+
+                    LoadWorkers();
+                    ClearFields();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Save Error: " + ex.Message);
+            }
+        }
+
+        private void dgvWorkers_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (dgvWorkers.CurrentRow != null)
+            {
+                txtFullName.Text = dgvWorkers.CurrentRow.Cells["FullName"].Value.ToString();
+                txtNIC.Text = dgvWorkers.CurrentRow.Cells["NIC"].Value.ToString();
+                txtPhone.Text = dgvWorkers.CurrentRow.Cells["Phone"].Value.ToString();
+                txtAddress.Text = dgvWorkers.CurrentRow.Cells["Address"].Value.ToString();
+                dtpJoined.Value = Convert.ToDateTime(dgvWorkers.CurrentRow.Cells["DateJoined"].Value);
+            }
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DBConnection db = new DBConnection();
+
+                using (SqlConnection conn = db.GetConnection())
+                {
+                    conn.Open();
+
+                    string query = @"UPDATE Workers
+                             SET FullName=@name,
+                                 NIC=@nic,
+                                 Phone=@phone,
+                                 Address=@address,
+                                 DateJoined=@date
+                             WHERE WorkerID=@id";
+
+                    SqlCommand cmd = new SqlCommand(query, conn);
+
+                    cmd.Parameters.AddWithValue("@id", dgvWorkers.CurrentRow.Cells["WorkerID"].Value);
+                    cmd.Parameters.AddWithValue("@name", txtFullName.Text);
+                    cmd.Parameters.AddWithValue("@nic", txtNIC.Text);
+                    cmd.Parameters.AddWithValue("@phone", txtPhone.Text);
+                    cmd.Parameters.AddWithValue("@address", txtAddress.Text);
+                    cmd.Parameters.AddWithValue("@date", dtpJoined.Value);
+
+                    cmd.ExecuteNonQuery();
+
+                    MessageBox.Show("Worker Updated");
+
+                    LoadWorkers();
                 }
             }
             catch (Exception ex)
@@ -59,14 +144,33 @@ namespace TeaEstateSystem
             }
         }
 
-        private void label1_Click(object sender, EventArgs e)
+        private void btnDelete_Click(object sender, EventArgs e)
         {
+            try
+            {
+                DBConnection db = new DBConnection();
 
-        }
+                using (SqlConnection conn = db.GetConnection())
+                {
+                    conn.Open();
 
-        private void label3_Click(object sender, EventArgs e)
-        {
+                    string query = "DELETE FROM Workers WHERE WorkerID=@id";
 
+                    SqlCommand cmd = new SqlCommand(query, conn);
+
+                    cmd.Parameters.AddWithValue("@id", dgvWorkers.CurrentRow.Cells["WorkerID"].Value);
+
+                    cmd.ExecuteNonQuery();
+
+                    MessageBox.Show("Worker Deleted");
+
+                    LoadWorkers();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
     }
 }
